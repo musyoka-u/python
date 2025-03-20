6bc53aac81788bc1a5065eedc262aa816b47043d
caleb sammy <musyokas148@gmail.com>
2025-03-20 11:26:00 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+#variables
+# variables are used to store data
+age = 58
+print("i am",age,"years old")
+
+#example2
+#to find average of two numbers
+k = 40
+l = 60
+average = (k + l)/2
+print ("the average of k and l is",average)
+
+#example 3
+#to find the area of  a rectangle
+length = 20
+widith = 10
+area = length * widith
+print("the area of rectangle is",area)
+
+#example 4
+#to find out if a number is even or old
+number = 6
+if number % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is old")
