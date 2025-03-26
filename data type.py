c0a86c47ff9310474454f8e2329cffb4de7f7507
caleb sammy <musyokas148@gmail.com>
2025-03-26 10:01:21 -0700

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,24 @@
+# python data
+# data in python is stored in different forms
+#example
+float = 34.5 # stores data of decimal point
+int = 10 # stores whole numbers data
+double =  3677.7#store large data in form of wholenumber
+string = "Hello, World!"  # stores text data
+
+#example
+int = 28
+print(f"i am {int} years old")
+
+#example 2
+float = 200.0
+print("i bought mangoes  of ksh",float)
+
+# example 3:
+double =16780.875
+print(f"the country supplied {double} grams of fertilizer  to the farmers during rain season")
+
+#example 4:
+string = 'kisasi_boys'
+print("the best student in KCSE this year was from",string)
+ 
